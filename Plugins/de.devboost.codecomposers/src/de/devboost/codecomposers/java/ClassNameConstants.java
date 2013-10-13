6eaa8679024786dcf0561c64e5e082b94cda152a
Mirko Seifert <mirko.seifert@devboost.de>
2013-10-13 13:28:27 +0200

    Added ClassNameConstants as alternative to IClassNameConstants

---
+++ Plugins/de.devboost.codecomposers/src/de/devboost/codecomposers/java/ClassNameConstants.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2013
+ * Software Technology Group, Dresden University of Technology
+ * DevBoost GmbH, Berlin, Amtsgericht Charlottenburg, HRB 140026
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Software Technology Group - TU Dresden, Germany;
+ *   DevBoost GmbH - Berlin, Germany
+ *      - initial API and implementation
+ ******************************************************************************/
+package de.devboost.codecomposers.java;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Constants for class names used in the generated code.
+ */
+public class ClassNameConstants {
+
+	public String ARRAY_LIST(JavaComposite jc) {
+		return jc.getClassName(ArrayList.class.getName());
+	}
+
+	public String LINKED_HASH_MAP(JavaComposite jc) {
+		return jc.getClassName(LinkedHashMap.class.getName());
+	}
+
+	public String LINKED_HASH_SET(JavaComposite jc) {
+		return jc.getClassName(LinkedHashSet.class.getName());
+	}
+
+	public String LIST(JavaComposite jc) {
+		return jc.getClassName(List.class.getName());
+	}
+
+	public String MAP(JavaComposite jc) {
+		return jc.getClassName(Map.class.getName());
+	}
+
+	public String MAP_ENTRY(JavaComposite jc) {
+		return jc.getClassName(Map.Entry.class.getCanonicalName());
+	}
+
+	public String SET(JavaComposite jc) {
+		return jc.getClassName(Set.class.getName());
+	}
+}
