fd11d4471824f7950200435c6480354b9b3835ad
Mirko Seifert <mirko.seifert@devboost.de>
2016-08-24 20:22:40 +0200

    - Added missing copyright header - Refactored code to use early exits and smaller methods

--- Plugins/de.devboost.codecomposers/src/de/devboost/codecomposers/csharp/CSharpImportsPlaceholder.java
+++ Plugins/de.devboost.codecomposers/src/de/devboost/codecomposers/csharp/CSharpImportsPlaceholder.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2016
+ * Software Technology Group, Dresden University of Technology
+ * DevBoost GmbH, Dresden, Amtsgericht Dresden, HRB 34001
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Software Technology Group - TU Dresden, Germany;
+ *   DevBoost GmbH - Dresden, Germany
+ *      - initial API and implementation
+ ******************************************************************************/
 package de.devboost.codecomposers.csharp;
 
 import java.util.ArrayList;
@@ -8,26 +23,38 @@ import de.devboost.codecomposers.java.Import;
 import de.devboost.codecomposers.java.ImportsPlaceholder;
 
 public class CSharpImportsPlaceholder extends ImportsPlaceholder {
+	
 	public CSharpImportsPlaceholder(String lineBreak) {
 		super(lineBreak);
 	}
 
 	@Override
 	public String getText() {
+		List<String> imports = getImports();
+		Collections.sort(imports);
+		return toString(imports);
+	}
+
+	private List<String> getImports() {
 		List<String> imports = new ArrayList<String>();
 		for (Import qualifiedImport : qualifiedImports) {
 			String qualifiedName = qualifiedImport.getQualifiedName();
 			int endIndex = qualifiedName.lastIndexOf('.');
-			if (endIndex > 0) {
-				String packageImport = qualifiedName.substring(0, endIndex);
-				if (!imports.contains(packageImport)) {
-					imports.add(packageImport);
-				}
+			if (endIndex <= 0) {
+				continue;
 			}
+
+			String packageImport = qualifiedName.substring(0, endIndex);
+			if (imports.contains(packageImport)) {
+				continue;
+			}
+			
+			imports.add(packageImport);
 		}
+		return imports;
+	}
 
-		Collections.sort(imports);
-
+	private String toString(List<String> imports) {
 		StringBuilder text = new StringBuilder();
 		for (String importToAdd : imports) {
 			text.append("using");
